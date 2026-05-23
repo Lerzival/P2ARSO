3240ef072fa40f632f641bee5d074aa525e48d8f
Lerzival <miguel.colomo@outlook.es>
2026-05-23 12:59:38 +0200

    Connectivity Check bugfix + revisión mj

--- connectivityCheck.py
+++ connectivityCheck.py
@@ -6,16 +6,19 @@ from functions import readFile
 from functions import exists
 
 def connectivityCheck():   
-    if not exists("servers.txt"):
-        logger.error("Error: no existe la red. Ejecuta 'create' primero.")
+    try:
+        num_servidores = readFile("servers.txt") 
+    except:
+        logger.error("Error: no existe la red. Ejecuta 'create' primero.") # Comprueba que los contenedores existan para poder eliminarlos
         return
     logger.info("Comprobando conectividad con lb:")
     subprocess.run(["lxc", "exec", "c1", "--", "ping", "-c", "2", "134.3.1.10"]) # Ping a lb desde c1
     
     num_servidores = int(readFile("servers.txt"))
-    for i in range(1, num_servidores + 1): 
-        logger.info("Comprobando conectividad con s" + str(i))
-        subprocess.run(["lxc", "exec", "c1", "--", "ping", "-c", "2",  "134.3.0.1" + str(i)])
+    for i in range (5):
+        if exists (f"s{i}"):
+            logger.info(f"Comprobando conectividad con s{i}")
+            subprocess.run(["lxc", "exec", "c1", "--", "ping", "-c", "2",  "134.3.0.1" + str(i)])
         
 #Somos conscientes de que en un escenario real, la transparencia del balanceador haria que no fuese necesario que el 
 #cliente supiera las IPs de los servidores. Realizamos la prueba de forma puramente informativa, para asegurarnos 
