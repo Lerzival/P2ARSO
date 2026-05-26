2a1453c51c8e37d22a498a6e279c2e92178c6c6b
gaizkurry <gaizka.lavado@gmail.com>
2026-05-26 11:15:43 +0200

    enlarge edit

--- enlarge.py
+++ enlarge.py
@@ -10,15 +10,19 @@ logger = logging.getLogger(__name__)
 
 def enlarge():
 
+   if readFile("configuration.txt") == 1:
+      logger.error("Para ejecutar enlarge es necesario configurar los servidores.")
+      return
+
    # Crear servidor 
-    logger.info("Creando nuevo servidor...")
-    server = createOne()
-    logger.info(f"{server} creado")
+   logger.info("Creando nuevo servidor")
+   server = createOne()
+   logger.info(f"{server} creado")
 
    # Iniciarlo 
-    startOne(server)
-    logger.info(f"{server} arrancado")
+   startOne(server)
+   logger.info(f"{server} arrancado")
 
    # Configurarlo 
-    configureOne(server)
-    logger.info(f"{server} configurado")
+   configureOne(server)
+   logger.info(f"{server} configurado")
